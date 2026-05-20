5dd326af0388b8d852af2606d71c34b394e28f50
Sriyans Kanneganti <srkanneg@calpoly.edu>
2026-05-19 23:11:34 -0700

    Finished

--- R/str_split_comma.R
+++ R/str_split_comma.R
@@ -1,3 +1,16 @@
+#' Split a string with commas
+#'
+#' @param string A character vector with, at most, one element with commas as the delimiter.
+#' @inheritParams stringr::str_split
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' str_split_comma(x)
+
+
 str_split_comma <- function(string) {
   str_split_one(string, pattern = ",")
 }
--- R/str_split_nums.R
+++ R/str_split_nums.R
@@ -1,3 +1,15 @@
+#' Split a string and return individual numbers
+#'
+#' @param string A character vector with, at most, one element with only numbers besides the commas.
+#' @inheritParams stringr::str_split
+#'
+#' @return A numeric vector that is split up.
+#' @export
+#'
+#' @examples
+#' x <- "1,2,3"
+#' str_split_num(x)
+
 str_split_nums <- function(string, pattern) {
   result <- str_split_one(string, pattern)
   as.numeric(result)
